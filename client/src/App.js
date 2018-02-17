f426da274969da5da56d9597031dad9feb85614a
theoJA <theophilus.ja@gmail.com>
2018-02-17 17:09:08 -0500

    Added navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,48 @@
 import React, { Component } from 'react';
+import SideNav from "./components/SideNav";
+
 import logo from './logo.svg';
 import './App.css';
-import Map from './map';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      isNavActive: true
+    }
+  }
+
+  onSideNavClick() {
+    if (this.state.isNavActive) {
+      this.setState({ isNavActive: false })
+    } else {
+      this.setState({ isNavActive: true })
+    }
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to Reactttttt</h1>
-        </header>
-        <Map />
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="wrapper">
+        <SideNav isNavActive={this.state.isNavActive} />
+        
+        <div className="App">
+
+          <div id="content" onClick={this.onSideNavClick.bind(this)}>
+            <button type="button" id="sidebarCollapse" className="btn btn-info navbar-btn">
+              <i className="glyphicon glyphicon-align-left"></i>
+              Toggle Sidebar
+            </button>
+          </div>
+
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Welcome to React</h1>
+          </header>
+          <p className="App-intro">
+            To get started, edit <code>src/App.js</code> and save to reload.
+          </p>
+        </div>
       </div>
     );
   }
