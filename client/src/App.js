5c5fac581b9ffee77911bd98c6f428ca9d7f7ae5
theoJA <theophilus.ja@gmail.com>
2018-02-17 18:49:49 -0500

    fixed map rendering issue

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import SideNav from "./components/SideNav";
+import Map from './map';
 
 import logo from './logo.svg';
 import './App.css';
@@ -42,6 +43,7 @@ class App extends Component {
           <p className="App-intro">
             To get started, edit <code>src/App.js</code> and save to reload.
           </p>
+          <Map /> 
         </div>
       </div>
     );
--- client/src/map.js
+++ client/src/map.js
@@ -3,18 +3,37 @@ import Brush from './Models/brush';
 
 
 class Map extends Component {
-    componentDidMount(){
-        let brush = new Brush(this.context);
-        brush.drawRect(100, 100, 50 , 50);
-    }
+
+  componentDidMount() {
+    this.updateCanvas();
+}
+  updateCanvas() {
+    const ctx = this.refs.canvas.getContext('2d');
+    //ctx.fillRect(100,100, 50, 50);
+    
+    let brush = new Brush(ctx);
+    brush.drawRect(100, 100, 50 , 50);
+
+        
+  }
+    // componentDidMount(){
+    //     let brush = new Brush(this.context);
+    //     brush.drawRect(100, 100, 50 , 50);
+    // }
 
   render() {
     return (
       <div className="Map">
-        <canvas is="canvasMap" ref={(c) => this.context = c.getContext('2d')} height="450" width="600" alt="My Map"></canvas>
+        <canvas is="canvasMap"
+          ref="canvas"
+          height="450"
+          width="600"
+          alt="My Map">
+        </canvas>
       </div>
     );
   }
 }
 
 export default Map;
+//ref={(c) => this.context = c.getContext('2d')}
