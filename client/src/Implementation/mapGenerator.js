ad5d97e28a1c6a650923e03b276d5820fdb7030f
Naragod <mnaranjo@phreesia.com>
2018-02-18 01:58:32 -0500

    clickable hexes

--- client/src/Implementation/mapGenerator.js
+++ client/src/Implementation/mapGenerator.js
@@ -1,23 +1,30 @@
-import React, { Component } from 'react';
-
 import Hexagon from '../Models/hexagon';
-import Brush from '../Models/brush';
+import hexagon from '../Models/hexagon';
 
 
 class MapGenerator{
     constructor(){
         this.edgeLength = 60;
         this.hexagons = this.getHexagons();
+        //this.hexagons = this.generate();
+    }
+
+    generate(){
+        let hex1 = new Hexagon([100, 100], 60);
+        let hex2 = new Hexagon([    hex1._center[0] + hex1.width * 3 / 4,
+                                    hex1._center[1] + hex1.height/2], 60);
+            return [hex1, hex2]
+        
     }
 
     getHexagons(){
         let res = [];
         let xHex = new Hexagon([0,0], this.edgeLength);
-        let xOffset = xHex.width;
-        let yOffset = xHex.height;
+        let xOffset = xHex.width * 4/3;
+        let yOffset = xHex.height + this.edgeLength/2;
 
-        for(let x = 0; x < 3 * xOffset; x += xOffset){
-            for(let y = 0; y < 1 * yOffset; y += yOffset){
+        for(let x = 0; x < 10 * xOffset; x += xOffset){
+            for(let y = 0; y < 10 * yOffset; y += yOffset){
                 let hex = new Hexagon([x, y], this.edgeLength);
                 console.log("Hexagon:", hex);
                 res.push(hex);
