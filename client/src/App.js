72f64b917937ec1bb83759ffe78ca46f0d907f0f
theoJA <theophilus.ja@gmail.com>
2018-02-17 20:38:09 -0500

    made new menu button

--- client/src/App.js
+++ client/src/App.js
@@ -10,7 +10,7 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      isNavActive: true
+      isNavActive: false
     }
   }
 
@@ -23,6 +23,7 @@ class App extends Component {
   }
 
   render() {
+    const { isNavActive } = this.state;
     return (
       <div className="wrapper">
         <SideNav isNavActive={this.state.isNavActive} />
@@ -32,9 +33,8 @@ class App extends Component {
           <header className="App-header">
 
             <div id="content">
-              <button onClick={this.onSideNavClick.bind(this)} type="button" id="sidebarCollapse" className="btn btn-info navbar-btn">
-                <i className="glyphicon glyphicon-align-left"></i>
-                Toggle Sidebar
+              <button onClick={this.onSideNavClick.bind(this)} type="button" id="sidebarCollapse" className={isNavActive ? "btn btn-info navbar-btn menu-margin" : "btn btn-info navbar-btn"}>
+                <i className="fas fa-bars"></i>
               </button>
             </div>
 
