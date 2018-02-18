e1627dba0313236467ee5ea67c617c8eb2bf6053
theoJA <theophilus.ja@gmail.com>
2018-02-17 23:33:11 -0500

    recent

---
+++ client/src/components/Activities.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import '../styles/Activities.css';
+
+export const Activities = () => {
+
+    return (
+        <div className="green-container">
+            <div className="green-inner-container">
+                <div>
+                    <h1>My Activities</h1>
+                    <p>All your activities are listed in here. All your activities are listed in here. All your activities are listed in here. 
+                    All your activities are listed in here. All your activities are listed in here. All your activities are listed in here. 
+                    All your activities are listed in here. All your activities are listed in here. All your activities are listed in here. 
+                    All your activities are listed in here. All your activities are listed in here. All your activities are listed in here. 
+                    All your activities are listed in here. All your activities are listed in here. All your activities are listed in here. 
+                    All your activities are listed in here. All your activities are listed in here. All your activities are listed in here. 
+                    All your activities are listed in here. All your activities are listed in here. All your activities are listed in here.
+                    </p>
+                </div>
+            </div>
+        </div>
+    )
+}
