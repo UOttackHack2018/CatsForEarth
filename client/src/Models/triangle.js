402b3d0c716a6b2c26b521541a6d494bc0bca07d
Omar Almoallim <omaralmo@Omars-MacBook-Pro.local>
2018-02-18 08:00:34 -0500

    add triangle

---
+++ client/src/Models/triangle.js
@@ -0,0 +1,10 @@
+class Triangle{
+    constructor(x, y, base, height){
+        this.base = base;
+        this.height = height;
+        
+        this.area = (base * height) / 2();
+        }   
+}
+
+export default Triangle;
